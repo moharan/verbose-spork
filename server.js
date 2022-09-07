ed858d65359d13f26f29d8da940c8866e33f2c87
Rangi Becerra <rangibecerra@gmail.com>
2022-09-07 16:32:16 -0400

    mongoose

--- server.js
+++ server.js
@@ -2,17 +2,35 @@ const express = require("express");
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 
 const schema = require("./schema");
+const testSchema = require("./types_schema");
+const mongoose = require("mongoose");
 
 const server = express();
+const port = process.env.PORT || 4000
+
+// mongodb+srv://guia:<uLSDRs2rPnZr0u5Y>@sismoguiacluster.9ckofjt.mongodb.net/?retryWrites=true&w=majority
 
 server.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
-    schema: schema
+    // schema: schema
+    schema: testSchema
   })
 );
 
-server.listen(4000, () => {
-  console.log("Escuchando a mi puerto 4000");
-});
+// useCreateIndex:true
+mongoose.connect(
+  `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPasword}@sismoguiacluster.9ckofjt.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
+  {useNewUrlParser: true, useUnifiedTopology:true}
+).then(() => {
+  server.listen({port: port}, () => {
+    console.log("Escuchando a mi puerto 4000");
+  });
+}).catch((e) => {
+  console.log(process.env.mongoUserName),
+  console.log("Error:::" + e)});
+
+
+
+
--- types_schema.js
+++ types_schema.js
@@ -1,14 +1,16 @@
-const graphql = require('graphql');
+const graphql = require("graphql");
 
 const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLBoolean,
-    GraphQLFloat,
-    GraphQLNonNull
-} = graphql
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLNonNull,
+  GraphQLSchema
+
+} = graphql;
 
 // Scalar Type
 /*
@@ -20,42 +22,45 @@ const {
 */
 
 const Person = new GraphQLObjectType({
-    name: 'Person',
-    description: 'Represent a person type',
-    fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: new GraphQLNonNull(GraphQLString)},
-        age: {type:  new GraphQLNonNull(GraphQLInt)},
-        isMarried: {type:  new GraphQLNonNull(GraphQLBoolean)},
-        gpa: {type:  new GraphQLNonNull(GraphQLFloat)},
+  name: "Person",
+  description: "Represent a person type",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    age: { type: new GraphQLNonNull(GraphQLInt) },
+    isMarried: { type: new GraphQLNonNull(GraphQLBoolean) },
+    gpa: { type: new GraphQLNonNull(GraphQLFloat) },
 
-        justAType: {
-            type: Person,
-            resolve(parent, args) {
-                return parent;
-            }
-        }
-    })
+    justAType: {
+      type: Person,
+      resolve(parent, args) {
+        return parent;
+      },
+    },
+  }),
 });
 
 // RootQuery
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    description: 'Description',
-    fields: {
-        type: Person,
-        resolve(parent, args) {
-            let personObj = {
-                // id: {type: GraphQLID},
-                name: 'Antonio',
-                age: 34,
-                isMarried: true,
-                gpa: 4.0,
-            }
-        }
-    }
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    person: {
+      type: Person,
+      resolve(parent, args) {
+        let personObj = {
+          // id: {type: GraphQLID},
+          name: "Antonio",
+          age: 34,
+          isMarried: true,
+          gpa: 4.0,
+        };
+        return personObj;
+      },
+    },
+  },
 });
 
 module.exports = new graphql.GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
