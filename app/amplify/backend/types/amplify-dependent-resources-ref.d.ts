e410386b97b5b65fafbac2a2d5942a20e96be75d
Rangi Becerra <rangibecerra@gmail.com>
2022-09-08 16:22:14 -0400

    conected api with front on index.js

--- app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "appsismoguia": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
