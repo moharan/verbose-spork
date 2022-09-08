8233397f788068ee1dbf606ddde23d5095291e8d
Rangi Becerra <rangibecerra@gmail.com>
2022-09-08 00:55:19 -0400

    cors

--- schema.js
+++ schema.js
@@ -1,5 +1,5 @@
-const graphql = require('graphql');
-var _ = require('lodash');
+const graphql = require("graphql");
+var _ = require("lodash");
 
 // model
 const User = require("./model/user");
@@ -27,182 +27,187 @@ const Post = require("./model/post");
 // ]
 
 const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
-
-} = graphql
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
 
 //create types
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    description: '  Documentation for user...',
-    fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        age: {type: GraphQLInt},
-        profession: {type: GraphQLString},
+  name: "User",
+  description: "  Documentation for user...",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    profession: { type: GraphQLString },
 
-        posts: {
-            type: new GraphQLList(PostType),
-            resolve(parent, args) {
-                return _.filter(postData, {userId: parent.id})
-            }
-        },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve(parent, args) {
+        return Post.find({ userId: parent.id });
+        // return _.filter(postData, {userId: parent.id})
+      },
+    },
 
-        hobbies: {
-            type: new GraphQLList(HobbyType),
-            resolve(parent, args) {
-                return _.filter(hobbiesData, {userId: parent.id})
-            }
-        },
-    })
+    hobbies: {
+      type: new GraphQLList(HobbyType),
+      resolve(parent, args) {
+        return Hobby.find({ userId: parent.id });
+        // return _.filter(hobbiesData, {userId: parent.id})
+      },
+    },
+  }),
 });
 
 const HobbyType = new GraphQLObjectType({
-    name: 'Hobby',
-    description: '  Documentation for hobby...',
-    fields: () => ({
-        id: {type: GraphQLID},
-        title: {type: GraphQLString},
-        description: {type: GraphQLInt},
-        userId: {
-            type: UserType,
-            resolve(parent, args) {
-                return _.find(usersData, {id: parent.userId})
-            }
-        }
-    })
+  name: "Hobby",
+  description: "  Documentation for hobby...",
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLInt },
+    userId: {
+      type: UserType,
+      resolve(parent, args) {
+        return User.findById({ userId: parent.id });
+        // return _.find(usersData, {id: parent.userId})
+      },
+    },
+  }),
 });
 
 const PostType = new GraphQLObjectType({
-    name: 'Post',
-    description: '  Documentation for POST...',
-    fields: () => ({
-        id: {type: GraphQLID},
-        comment: {type: GraphQLString},
-        userId: {
-            type: UserType,
-            resolve(parent, args) {
-                return _.find(usersData, {id: parent.userId})
-            }
-        }
-    })
+  name: "Post",
+  description: "  Documentation for POST...",
+  fields: () => ({
+    id: { type: GraphQLID },
+    comment: { type: GraphQLString },
+    userId: {
+      type: UserType,
+      resolve(parent, args) {
+        return User.findById({ userId: parent.id });
+        // return _.find(usersData, {id: parent.userId})
+      },
+    },
+  }),
 });
 
 //RootQuery
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    description: 'Description',
-    fields: {
-        user: {
-            type: UserType,
-            args: {id: {type: GraphQLString}},
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
 
-            resolve(parent, args){
-                return _.find(usersData, {id: args.id});
-                // return data from a datasource
-            }
-        },
-        users: {
-            type: new GraphQLList(UserType),
-            resolve(parent, args){
-                return usersData;
-            }
-        },
-        hobby:{
-            type: HobbyType,
-            args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return User.findById({ userId: parent.id });
+        // return _.find(usersData, {id: args.id});
+        // return data from a datasource
+      },
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parent, args) {
+        return User.find({});
+      },
+    },
+    hobby: {
+      type: HobbyType,
+      args: { id: { type: GraphQLID } },
 
-            resolve(parent, args){
-                return _.find(hobbiesData, {id: args.id});
-            }
-        },
-        hobbies: {
-            type: new GraphQLList(HobbyType),
-            resolve(parent, args){
-                return hobbiesData;
-            }
-        },
-        post: {
-            type: PostType,
-            args: {id: {type: GraphQLID}},
+      resolve(parent, args) {
+        return Hobby.findById(args.id);
+      },
+    },
+    hobbies: {
+      type: new GraphQLList(HobbyType),
+      resolve(parent, args) {
+        return Hobby.find({ id: args.userId });
+      },
+    },
+    post: {
+      type: PostType,
+      args: { id: { type: GraphQLID } },
 
-            resolve(parent, args){
-                return _.find(postData, {id: args.id});
-            }
-        },
-        posts: {
-            type: new GraphQLList(PostType),
-            resolve(parent, args){
-                return postData;
-            }
-        },
-    }
+      resolve(parent, args) {
+        return Post.findById(args.id);
+        // return _.find(postData, {id: args.id});
+      },
+    },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve(parent, args) {
+        return Post.find({});
+      },
+    },
+  },
 });
 
 //mutations
 const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-        CreateUser: {
-            type: UserType,
-            args: {
-                //id: {type: GraphQLID}
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt},
-                profession: {type: GraphQLString}
-            },
+  name: "Mutation",
+  fields: {
+    CreateUser: {
+      type: UserType,
+      args: {
+        //id: {type: GraphQLID}
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        profession: { type: GraphQLString },
+      },
 
-            resolve(parent, args) {
-                let user = User({
-                    name: args.name,
-                    age: args.age,
-                    profession: args.profession
-                });
-                return user.save();
-            }
-        },
-        CreatePost: {
-            type: PostType,
-            args: {
-                comment: {type: new GraphQLNonNull(GraphQLString)},
-                userId: {type: new GraphQLNonNull(GraphQLID)}
-            },
+      resolve(parent, args) {
+        let user = User({
+          name: args.name,
+          age: args.age,
+          profession: args.profession,
+        });
+        return user.save();
+      },
+    },
+    CreatePost: {
+      type: PostType,
+      args: {
+        comment: { type: new GraphQLNonNull(GraphQLString) },
+        userId: { type: new GraphQLNonNull(GraphQLID) },
+      },
 
-            resolve(parent, args) {
-                let post = Post({
-                    comment: args.comment,
-                    userId: args.userId
-                });
-                return post.save();
-            }
-        },
-        CreateHobby: {
-            type: HobbyType,
-            args: {
-                title: {type: GraphQLString},
-                description: {type: GraphQLString},
-                userId: {type: GraphQLID}
-            },
+      resolve(parent, args) {
+        let post = Post({
+          comment: args.comment,
+          userId: args.userId,
+        });
+        return post.save();
+      },
+    },
+    CreateHobby: {
+      type: HobbyType,
+      args: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        userId: { type: new GraphQLNonNull(GraphQLString) },
+      },
 
-            resolve(parent, args) {
-                let hobby = Hobby({
-                    title: args.title,
-                    description: args.description,
-                    userId: args.userId
-                });
-                return hobby.save();
-            }
-        }
-    }
-})
+      resolve(parent, args) {
+        let hobby = Hobby({
+          title: args.title,
+          description: args.description,
+          userId: args.userId,
+        });
+        return hobby.save();
+      },
+    },
+  },
+});
 
 module.exports = new graphql.GraphQLSchema({
-    query: RootQuery,
-    mutation: Mutation
-})
+  query: RootQuery,
+  mutation: Mutation,
+});
--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 const express = require("express");
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 const schema = require("./schema");
 const testSchema = require("./types_schema");
 const mongoose = require("mongoose");
 
 const server = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 server.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
-    schema: schema
+    schema: schema,
     // schema: testSchema
   })
 );
 
 // useCreateIndex:true
-mongoose.connect(
-  `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPasword}@sismoguiacluster.9ckofjt.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
-  {useNewUrlParser: true, useUnifiedTopology:true}
-).then(() => {
-  server.listen({port: port}, () => {
-    console.log("Escuchando a mi puerto 4000");
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPasword}@sismoguiacluster.9ckofjt.mongodb.net/${process.env.mongoDatabase}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    server.listen({ port: port }, () => {
+      console.log("Escuchando a mi puerto 4000");
+    });
+  })
+  .catch((e) => {
+    console.log(process.env.mongoUserName), console.log("Error:::" + e);
   });
-}).catch((e) => {
-  console.log(process.env.mongoUserName),
-  console.log("Error:::" + e)});
-
-
-
-
