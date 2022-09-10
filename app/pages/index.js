8fd23429db88375b2da184e732277dacc3784477
Rangi Becerra <rangibecerra@gmail.com>
2022-09-09 21:36:15 -0400

    main grid and brand

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { API } from "aws-amplify";
 import { listPosts } from "./../src/graphql/queries";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./../images/sismoguia.svg";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -17,18 +19,23 @@ export default function Home() {
     setPosts(postData.data.listPosts.items);
   }
   return (
-    <>
-      <h1 className="">Inicio</h1>
-      {posts.map((post, index) => (
-        <Link key={index} href={`/posts/${post.id}`}>
-          <div>
-            <h2 className="" key={index}>
-              {post.title}
-            </h2>
-            <p>Autor: {post.username}</p>
-          </div>
-        </Link>
-      ))}
-    </>
+    <div className="contain-page">
+      <div className="logo">
+        <Image src={Logo} layout="responsive" width={500} height={500} />
+      </div>
+      <div>
+        <h1 className="">Inicio</h1>
+        {posts.map((post, index) => (
+          <Link key={index} href={`/posts/${post.id}`}>
+            <div>
+              <h2 className="" key={index}>
+                {post.title}
+              </h2>
+              <p>Autor: {post.username}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   );
 }
