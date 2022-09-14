2b78511ae9e4191105dba4325148f90e0c3540f0
Rangi Becerra <rangibecerra@gmail.com>
2022-09-14 15:22:47 -0300

    Fixed info details

--- app/pages/index.js
+++ app/pages/index.js
@@ -31,28 +31,30 @@ export default function Home() {
         className="event-card"
         extra={
           <div className="bg-card">
-            <Popover content={"cuidado"} title="Medidas Precaución">
+            <Popover content={<a href="#">Ver más...</a>} title="Medidas Precaución">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-                <Image src={Cuidado}/>
+                <Image src={Cuidado} />
               </a>
             </Popover>
             <br />
-            <Popover content={"- Durante el Sismo"} title="Información">
+            <Popover content={<a href="#">Ver más...</a>} title="Durante un Sismo">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-              <Image src={Durante}/>
+                <Image src={Durante} />
               </a>
             </Popover>
             <br />
-            <Popover content={"- Después del Sismo"} title="Información">
+            <Popover content={<a href="#">Ver más...</a>} title="Después del Sismo">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-              <Image src={Despues}/>
+                <Image src={Despues} />
               </a>
             </Popover>
           </div>
         }
       >
         <p>2022-09-14</p>
-        <p><b>13:39:18</b></p>
+        <p>
+          <b>13:39:18</b>
+        </p>
         <p>49 km al SO de Mina Collahuasi</p>
         <p>116 km</p>
         <h4>2.8</h4>
@@ -63,28 +65,30 @@ export default function Home() {
         className="event-card"
         extra={
           <div className="bg-card">
-            <Popover content={"cuidado"} title="Medidas Precaución">
+            <Popover content={<a href="#">Ver más...</a>} title="Medidas Precaución">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-                <Image src={Cuidado}/>
+                <Image src={Cuidado} />
               </a>
             </Popover>
             <br />
-            <Popover content={"- Durante el Sismo"} title="Información">
+            <Popover content={<a href="#">Ver más...</a>} title="Durante un Sismo">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-              <Image src={Durante}/>
+                <Image src={Durante} />
               </a>
             </Popover>
             <br />
-            <Popover content={"- Después del Sismo"} title="Información">
+            <Popover content={<a href="#">Ver más...</a>} title="Después del Sismo">
               <a href="/#" onClick={(e) => e.preventDefault()}>
-              <Image src={Despues}/>
+                <Image src={Despues} />
               </a>
             </Popover>
           </div>
         }
       >
         <p>2022-09-13 </p>
-        <p><b>16:57:27</b></p>
+        <p>
+          <b>16:57:27</b>
+        </p>
         <p>55 km al SE de Antofagasta</p>
         <p>60 KM</p>
         <h4>4.1</h4>
@@ -93,23 +97,24 @@ export default function Home() {
       {posts.map((post, index) => (
         <Link key={index} href={`/posts/${post.id}`}>
           <Card className="report-card">
-            <p className="detail-title" key={index}>{post.title}</p>
-            <p className="detail-info"><b>Autor:</b> {post.username}</p>
-            {
-              post.comments.items.length > 0 &&
-              post.comments.items.map((comments,index) => (
+            <p className="detail-title" key={index}>
+              {post.title}
+            </p>
+            <p className="detail-info">
+              <b>Autor:</b> {post.username}
+            </p>
+            {post.comments.items.length > 0 &&
+              post.comments.items.map((comments, index) => (
                 <div key={index}>
                   <div className="comment-message">
-                    <p>
-                      {comments.message}
-                    </p>
+                    <p>{comments.message}</p>
                     <p className="detail-info">
-                      <span className="detail-by">Comentario por:</span> {comments.createdBy}
+                      <span className="detail-by">Comentario por:</span>{" "}
+                      {comments.createdBy}
                     </p>
                   </div>
                 </div>
-              ))
-            }
+              ))}
           </Card>
         </Link>
       ))}
